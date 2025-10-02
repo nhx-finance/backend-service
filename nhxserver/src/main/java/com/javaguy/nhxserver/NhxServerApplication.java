b943c1e3d31c330464c0f037eeb77f3d7e50330e
Dancan254 <dancanian25@gmail.com>
2025-10-02 15:15:47 +0300

    Enable asynchronous processing in NhxServerApplication and change main method visibility to public.

--- nhxserver/src/main/java/com/javaguy/nhxserver/NhxServerApplication.java
+++ nhxserver/src/main/java/com/javaguy/nhxserver/NhxServerApplication.java
@@ -2,11 +2,17 @@ package com.javaguy.nhxserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @SpringBootApplication
+@EnableAsync
 public class NhxServerApplication {
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(NhxServerApplication.class, args);
     }
 
