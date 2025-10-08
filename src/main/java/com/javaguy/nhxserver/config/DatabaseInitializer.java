cc41c1ecd6d759e4d5d12ecb6f0f873a59e97349
Dancan254 <dancanian25@gmail.com>
2025-10-08 10:59:35 +0300

    add migration scripts

--- src/main/java/com/javaguy/nhxserver/config/DatabaseInitializer.java
+++ src/main/java/com/javaguy/nhxserver/config/DatabaseInitializer.java
@@ -10,10 +10,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.transaction.annotation.Transactional;
 
 @Configuration
 @RequiredArgsConstructor
+@Profile("!prod")
 public class DatabaseInitializer implements CommandLineRunner {
 
         private static final Logger log = LoggerFactory.getLogger(DatabaseInitializer.class);
