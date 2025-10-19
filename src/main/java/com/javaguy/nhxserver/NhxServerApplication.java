7d0bc14fb9e15312d87d6e93c91a4a51b867b939
Dancan254 <dancanian25@gmail.com>
2025-10-19 16:58:50 +0300

    Refactor imports in NhxServerApplication and DatabaseInitializer for clarity

--- src/main/java/com/javaguy/nhxserver/NhxServerApplication.java
+++ src/main/java/com/javaguy/nhxserver/NhxServerApplication.java
@@ -4,9 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+
 
 @SpringBootApplication
 @EnableAsync
