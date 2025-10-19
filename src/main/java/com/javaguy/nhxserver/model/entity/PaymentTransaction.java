db7d55bb83b14c7b9b83ad27728c57a0e77c64b7
Dancan254 <dancanian25@gmail.com>
2025-10-20 00:09:33 +0300

    Remove unused status field from PaymentTransactionDto and add AllArgsConstructor to PaymentTransaction entity for improved functionality.

--- src/main/java/com/javaguy/nhxserver/model/entity/PaymentTransaction.java
+++ src/main/java/com/javaguy/nhxserver/model/entity/PaymentTransaction.java
@@ -1,6 +1,7 @@
 package com.javaguy.nhxserver.model.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,6 +11,7 @@ import java.time.LocalDateTime;
 @Entity
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class PaymentTransaction {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
